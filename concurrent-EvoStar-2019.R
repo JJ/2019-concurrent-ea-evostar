8625d545b491a85510574c4d96f18f82bdb56792
JJ Merelo <jjmerelo@gmail.com>
2019-04-23 18:34:12 +0200

    Remakes images

--- concurrent-EvoStar-2019.R
+++ concurrent-EvoStar-2019.R
@@ -6,17 +6,17 @@ data <- read.csv("data/evostar2019.csv")
 data$gap = as.factor(data$gap)
 
 ## ----results-evals,cache=FALSE,echo=FALSE,fig.height=4-------------------
-ggplot(data,aes(x=evaluations,y=time,color=gap))+scale_color_brewer(palette="Set1")+geom_point(size=3,aes(shape=gap))+theme_tufte()+labs(x="Evaluations",y="Time",title="Evaluations vs Time per generation gap")
+ggplot(data,aes(x=evaluations,y=time,color=gap))   + scale_color_brewer(palette="Set1")  + geom_point(size=3,aes(shape=gap))+theme_tufte()  + theme(axis.text.y = element_text(size = 20),axis.text.x = element_text(size = 20))  + labs(x="Evaluations",y="Time",title="Evaluations vs Time per generation gap")
 ggsave("docs/img/results-evals.png", height=9, width=16)
 
 ## ----results-evals-bp,cache=FALSE,echo=FALSE-----------------------------
-ggplot(data,aes(x=gap,y=evaluations,group=gap))+geom_boxplot()+theme_tufte()+labs(title="Evaluations vs generation gap")
+ggplot(data,aes(x=gap,y=evaluations,group=gap))+geom_boxplot()+theme_tufte()+ theme(axis.text.y = element_text(size = 20),axis.text.x = element_text(size = 20)) +labs(title="Evaluations vs generation gap")
 ggsave("docs/img/results-evals-bp.png", height=9, width=16)
 
 ## ----results-avg-evals,cache=FALSE,echo=FALSE,fig.height=4---------------
-ggplot(data,aes(x=gap,y=avg.eval,group=gap))+geom_boxplot()+theme_tufte()+labs(title="Average number of evaluations vs generation gap")
+ggplot(data,aes(x=gap,y=avg.eval,group=gap))+geom_boxplot()+theme_tufte()+ theme(axis.text.y = element_text(size = 20),axis.text.x = element_text(size = 20)) +labs(title="Average number of evaluations vs generation gap")
 ggsave("docs/img/results-avg-evals.png", height=9, width=16)
 
 ## ----results-avg-time,cache=FALSE,echo=FALSE,fig.height=4----------------
-ggplot(data,aes(x=gap,y=time,group=gap))+geom_boxplot()+theme_tufte()+labs(title="Time vs generation gap")
+ggplot(data,aes(x=gap,y=time,group=gap))+geom_boxplot()+theme_tufte()+ theme(axis.text.y = element_text(size = 20),axis.text.x = element_text(size = 20)) +labs(title="Time vs generation gap")
 ggsave("docs/img/results-avg-time.png", height=9, width=16)
